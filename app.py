7a00954837a7e4ef9689392e5822e4ba6e66dfcf
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-22 12:17:54 +0530

    Update app.py
    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/index.html',methods=['POST'])
+@app.route('/index.html', methods=['POST'])
 def generate_content():
     user_input = request.form.get('user_input')
 
@@ -16,23 +16,18 @@ def generate_content():
         "brand_id": "64a534aab378eaae3f312b25",
         "text": user_input,
         "media_type": "single_image",
-        #"template_ids": ["1"],
-  }
+    }
     headers = {"Authorization": "VpHFDiBO9sIB7q4KRrtdiABJAxJgBAcm"}
 
     response = requests.post(predis_ai_url, data=payload, headers=headers)
-    json_response = response.json()
-    
-    return json_response
 
-def show():
-    data = request.get_json(generate_content())
-
-    caption = data.get("caption")[0].get("caption"),
-    generated_media = data.get("generated_media")[0].get("url")
-    
-    return jsonify(caption = caption, image = generated_media)
-
+    if response.status_code == 200:
+        data = response.json()
+        caption = data.get("caption")[0].get("caption")
+        generated_media = data.get("generated_media")[0].get("url")
+        return jsonify(caption=caption, image=generated_media)
+    else:
+        return jsonify(error="Failed to generate content"), 500
 
 
 if __name__ == '__main__':
