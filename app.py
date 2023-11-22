5587154276721dab040c2aac9df3690bf5736ec1
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-22 18:53:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import requests
 
 app = Flask(__name__)
 
+# Configured webhook URL for saving responses
+save_response_url = "https://webhook-test-f8cd.onrender.com/index.html"
+
 @app.route('/')
 def index():
     return render_template('index.html')
     
-
 def generate_content(user_input):
     predis_ai_url = "https://brain.predis.ai/predis_api/v1/create_content/"
     payload = {
@@ -21,15 +23,40 @@ def generate_content(user_input):
 
     return response
 
-@app.route('/index.html', methods=['POST'])
-def display():
-    user_input = request.form.get('user_input')
-    response = generate_content(user_input)
+def save_to_configured_url(data):
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(save_response_url, json=data, headers=headers)
 
+    return response
+
+def fetch_and_save_response():
+    response = requests.get(save_response_url)
+
+    # Assuming the response is JSON, you can save it or process it accordingly
     if response.status_code == 200:
         data = response.json()
+        # Save or process the data as needed
+        print("Fetched response:", data)
+    else:
+        print("Failed to fetch response from the configured URL")
+
+    return response
+
+@app.route('/', methods=['POST'])
+def display():
+    user_input = request.form.get('user_input')
+    result = generate_content(user_input)
+
+    if result.status_code == 200:
+        data = result.json()
         caption = data.get("caption")[0].get("caption")
         generated_media = data.get("generated_media")[0].get("url")
+
+        # Save the response to the configured URL
+        save_to_configured_url({"caption": caption, "image": generated_media})
+
+        # Fetch and save the response from the configured URL
+        fetch_and_save_response()
 
         return jsonify(caption=caption, image=generated_media)
 
