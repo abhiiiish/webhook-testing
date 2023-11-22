d34c6b9563a1246d030186bca466d86db46c8c03
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-22 15:31:45 +0530

    Update app.py
    updated

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import requests
 
 app = Flask(__name__)
 
+# Configured webhook URL
+webhook_url = "https://webhook-test-f8cd.onrender.com/index.html"
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,11 +28,37 @@ def generate_content():
         data = response.json()
         caption = data.get("caption")[0].get("caption")
         generated_media = data.get("generated_media")[0].get("url")
+
+        # Save the POST response to the configured webhook URL
+        save_to_webhook(caption, generated_media)
+
         return jsonify(caption=caption, image=generated_media)
     else:
         return jsonify(error="Failed to generate content"), 500
 
+def save_to_webhook(caption, generated_media):
+    # Prepare data to send to the webhook
+    webhook_data = {
+        "caption": caption,
+        "image": generated_media
+    }
+
+    # Send POST request to the webhook URL
+    webhook_response = requests.post(webhook_url, json=webhook_data)
+
+    # Print the webhook response for debugging (you can handle this differently)
+    print("Webhook Response:", webhook_response.text)
+
+
+@app.route('/get_saved_data')
+def get_saved_data():
+    # You might want to retrieve the saved data from your database or another source
+    saved_data = {
+        "caption": "Saved Caption",
+        "image": "https://example.com/saved_image.jpg"
+    }
+    return jsonify(saved_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
