83de2177d5b8dc5e52bdf9dd2484a871dcf28a3c
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-22 15:35:06 +0530

    Update main.js
    added

--- static/main.js
+++ static/main.js
@@ -1,3 +1,13 @@
+// nav bar toggle button
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Fetch the saved data from the server when the page loads
+$(document).ready(function () {
+    $.get("/get_saved_data", function (data) {
+        // Update the content with saved data
+        $("#savedCaption").text(data.caption);
+        $("#savedImage").attr("href", data.image).text(data.image);
+    });
 });
