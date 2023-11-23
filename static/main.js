d4f9f900b094baac4918efe5ace9b4f2ad3e405b
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-23 13:07:52 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,19 +1,24 @@
-function generateContent() {
-    var userInput = document.getElementById('userInput').value;
+ function generateContent() {
+        var userInput = document.getElementById('userInput').value;
 
-    // Make an AJAX request to Flask backend
-    $.ajax({
-        type: 'POST',
-        url: '/index',
-        data: { user_input: userInput },
-        success: function (response) {
-            // Update the response text
-            document.getElementById('responseText').innerText = response;
-            // Show the response div
-            document.getElementById('responseDiv').style.display = 'block';
-        },
-        error: function (error) {
-            console.error('Error:', error);
-        }
-    });
-}
+        // Make an AJAX request to Flask backend
+        $.ajax({
+            type: 'POST',
+            url: '/index',
+            data: {user_input: userInput},
+            success: function(response) {
+                // Parse the JSON response
+                var responseData = JSON.parse(response);
+
+                // Update the response text and media URL
+                document.getElementById('responseText').innerText = 'Caption: ' + responseData.text;
+                document.getElementById('mediaUrl').innerText = 'Media URL: ' + responseData.url;
+
+                // Show the response div
+                document.getElementById('responseDiv').style.display = 'block';
+            },
+            error: function(error) {
+                console.error('Error:', error);
+            }
+        });
+    }
