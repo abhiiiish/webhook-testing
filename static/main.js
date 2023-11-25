d1cbaa06bab2660a166cc02c6650ec7a457f3aa3
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-25 10:25:23 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -4,7 +4,7 @@ function generateContent() {
     // Make an AJAX request to Flask backend
     $.ajax({
         type: 'POST',
-        url: 'https://socify-wofb.onrender.com/',
+        url: 'https://socify-wofb.onrender.com/webhook',
         data: { user_input: userInput },
         success: function (response) {
             // Update the response text
@@ -17,3 +17,34 @@ function generateContent() {
         }
     });
 }
+
+
+const webhook = "https://socify-wofb.onrender.com/webhook";
+
+function fetchWebhook() {
+    fetch(webhook)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error('Failed to fetch data');
+            }
+        })
+        .then(data => {
+            // Update the webpage with the fetched data
+            updateWebPage(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function updateWebPage(data) {
+    // Assuming you have an element with id 'webhookData' to display the data
+    const webhookDataElement = document.getElementById('webhookData');
+
+    // Update the content of the element with the fetched data
+    webhookDataElement.innerText = JSON.stringify(data, null, 2);
+}
+
+// Call the fetchWebhook function when the page loads or when needed
