e295711a09256bea7546014f51952a0cf8e06839
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-25 10:26:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify, abort
+import requests
 
 app = Flask(__name__)
 
@@ -6,35 +7,35 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/index.html', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def generate_content():
     try:
         user_input = request.form.get('user_input', '')  # Get user_input from the form
 
         predis_ai_url = "https://brain.predis.ai/predis_api/v1/create_content/"
         payload = {
-            "brand_id": "655b548ce8c5caef9d9455b6",
+            "brand_id": "65609012ab1e2fa91d82cce9",
             "text": user_input,
             "media_type": "video",
             "video_duration": "long",
         }
-        headers = {"Authorization": "RmntK5qqhRFlg6mcUDmLg4xArPINE6fv"}
-        response = requests.post(predis_ai_url, data=payload, headers=headers)
-        
-        return response.text 
-    
+        headers = {"Authorization": "ObzQ8v2MtY13bSB4vOGUa87kovoGYWDc"}
 
+        response = requests.post(predis_ai_url, data=payload, headers=headers)
+        text = response.text 
+        return jsonify(text = text)
+    
     except Exception as e:
         abort(500)
 
 def result():
     if request.method == 'POST':
-        data = request.json()
-        print("Received data:", data)  # Print data to the console
+        pritn( request.get_json())
 
-        return jsonify({'status': 'success'}), 200
-    else:
+        return 'success', 200
+    else :
         abort(400)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
