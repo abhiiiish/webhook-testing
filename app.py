3be0330288a0241d6fe67e896c766edd4d5a42ed
Abhishek Jain <124518416+abhiiiish@users.noreply.github.com>
2023-11-28 13:04:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-from flask import Flask, render_template, request, jsonify, abort
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import json, requests
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-
-with app.app_context():
-    db.create_all()
+migrate = Migrate(app, db)
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,35 +23,32 @@ class Profile(db.Model):
     def __repr__(self):
         return f"Profile(id={self.id}, brand_id={self.brand_id}, post_id={self.post_id}, caption={self.caption[:50]}, status={self.status}, media_url={self.media_url}, media_type={self.media_type})"
 
-
-migrate = Migrate(app, db)
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/db', methods=['POST'])
+def get_db_data():
+    profiles = Profile.query.all()
+    return jsonify({'profiles': profiles})
 
-@app.route('/index.html', methods=['POST'])
+@app.route('/index', methods=['POST'])
 def generate_content():
     user_input = request.form.get('user_input')
 
     url = "https://brain.predis.ai/predis_api/v1/create_content/"
     payload = {
         "brand_id": "64e86b35de4590305093c2b3",
-        "text": "3 tips for a healthy morning breakfast",
+        "text": user_input,  # Use user input here
         "media_type": "single_image",
         "color_palette_type": "ai_suggested"
     }
     headers = {"Authorization": "ayUWiVk7cLb8UR6aWNrDzeh5k41tU3cF"}
     response = requests.post(url, data=payload, headers=headers)
 
-    profiles = Profile.query.all()
+    return jsonify({'text': response.json})
 
-    return jsonify({'text': response.json, 'profiles': profiles})
-
-
-# Add a route to receive the webhook data
-@app.route('/', methods=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook_data():
     data = request.json
 
@@ -73,10 +66,5 @@ def webhook_data():
 
     return jsonify({"message": "Webhook data received and saved successfully"})
 
-
 if __name__ == '__main__':
-    app.run(Debug=True)
-
-app.app_context().push()
-
-
+    app.run(debug=True)
